8207d0970345aa3034b648126a3028c260a86e98
calboo <cal@rabooboo.de>
2025-11-30 20:25:35 +0100

    dependency update

--- Server/FastKeyValueServer/build.gradle.kts
+++ Server/FastKeyValueServer/build.gradle.kts
@@ -11,6 +11,19 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    // Ktor server dependencies
+    implementation("io.ktor:ktor-server-netty:2.3.4")
+    implementation("io.ktor:ktor-server-core:2.3.4")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.4")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
+
+    //Ktor Websockets and CORS
+    implementation("io.ktor:ktor-server-websockets:2.3.4")
+    implementation("io.ktor:ktor-server-cors:2.3.4")
+    implementation("io.ktor:ktor-client-core:2.3.4")
+    implementation("io.ktor:ktor-client-cio:2.3.4")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
 }
 
 tasks.test {
